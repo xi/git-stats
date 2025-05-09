3a5b51e2e6228943166355ae763d0e48ca6e024f
Tobias Bengfort <tobias.bengfort@posteo.de>
2025-05-09 08:01:58 +0200

    rm shortstat

--- shortstat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-git rev-list HEAD --no-merges | while read commit; do
-    printf '%s,' "$(git show -s --format='%cs')"
-    git show --shortstat "$commit" | tail -n 1 | sed 's/ *\([0-9]\+\) files\? changed\(, \([0-9]\+\) insertions\?(+)\)\?\(, \([0-9]\+\) deletions\?(-)\)\?/\1,\3,\5/'
-done
